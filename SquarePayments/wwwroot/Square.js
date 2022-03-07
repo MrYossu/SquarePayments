1c18e8ae13d3dc048c79a5400eb556cd0e825c19
Avrohom Yisroel Silver <mryossu@hotmail.com>
2022-03-07 22:35:14 +0000

    Add project files.

---
+++ SquarePayments/wwwroot/Square.js
@@ -0,0 +1,14 @@
+﻿export async function addSquareCardPayment(element, appId, locationId) {
+  const payments = Square.payments(appId, locationId);
+  const card = await payments.card();
+  await card.attach("#" + element);
+  return card;
+}
+export async function getSquareCardToken(card) {
+  const tokenResult = await card.tokenize();
+  if (tokenResult.status === 'OK') {
+    return tokenResult.token;
+  } else {
+    return null;
+  }
+}
