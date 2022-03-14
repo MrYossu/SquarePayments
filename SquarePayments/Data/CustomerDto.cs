b9ce6ebf11a3bb86a1b11c0dca4d6b727935da14
Avrohom Yisroel Silver <mryossu@hotmail.com>
2022-03-14 20:52:16 +0000

    You can now create a new customer

---
+++ SquarePayments/Data/CustomerDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SquarePayments.Data; 
+
+public class CustomerDto {
+  public string FirstName { get; set; } = "";
+  public string Surname { get; set; } = "";
+  [EmailAddress]
+  public string Email { get; set; } = "";
+  public string Phone { get; set; } = "";
+  public string Address1 { get; set; } = "";
+  public string Address2 { get; set; } = "";
+  public string Postcode { get; set; } = "";
+  public string Country { get; set; } = "GB";
+}
