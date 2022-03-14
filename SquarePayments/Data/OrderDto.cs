5972af5c5c39e3cb9afc2f0fa292080fe767fdb2
Avrohom Yisroel Silver <mryossu@hotmail.com>
2022-03-14 22:44:14 +0000

    You can now place and view orders, although I haven't worked out how to pay for them!

---
+++ SquarePayments/Data/OrderDto.cs
@@ -0,0 +1,11 @@
+﻿using Square.Models;
+
+namespace SquarePayments.Data; 
+
+public class OrderDto {
+  public string LocationId { get; set; } = "";
+  public string CustomerId { get; set; } = "";
+  public OrderLineItem[] Items { get; set; }
+  public string Reference { get; set; } = "";
+
+}
