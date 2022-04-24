1efe50b64cb1d964cf3e3f08c86f5ef84999bb13
Avrohom Yisroel Silver <mryossu@hotmail.com>
2022-04-24 17:02:47 +0100

    Updated Payments.razor to handle SCA

--- SquarePayments/wwwroot/Square.js
+++ SquarePayments/wwwroot/Square.js
@@ -4,6 +4,7 @@
   await card.attach("#" + element);
   return card;
 }
+
 export async function getSquareCardToken(card) {
   const tokenResult = await card.tokenize();
   if (tokenResult.status === 'OK') {
@@ -11,4 +12,29 @@ export async function getSquareCardToken(card) {
   } else {
     return null;
   }
+}
+
+export async function verifyTheBuyer(appId, locationId, token, amount, contact) {
+  // amount: string of the amount, eg '1.00'
+  // contact: details of the buyer, eg...
+  /*
+   {
+      addressLines: ['123 Main Street', 'Apartment 1'],
+      familyName: 'Doe',
+      givenName: 'John',
+      email: 'jondoe@gmail.com',
+      country: 'GB',
+      phone: '3214563987',
+      region: 'LND',
+      city: 'London',
+    }
+   */
+  const payments = Square.payments(appId, locationId);
+  const verificationResults = await payments.verifyBuyer(token, {
+    amount: amount,
+    billingContact: contact,
+    currencyCode: 'GBP', // Hard-coded to GBP. You may need to change this
+    intent: 'CHARGE'
+  });
+  return verificationResults.token;
 }
